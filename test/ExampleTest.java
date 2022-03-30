b78b3c6a658838a7760597c05c8b7cfc9e1c5e9b
adr1an0s0ar3s <adrianosoares35@gmail.com>
2022-03-30 15:14:32 +0100

    :pencil2: Work from 29/03

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -6,13 +6,19 @@ public class ExampleTest {
 
     @Test
     public void testExpression() {
-        TestUtils.parse("2+3\n10+20\n");
-        // var parserResult = TestUtils.parse("2+3\n10+20\n");
-        // parserResult.getReports().get(0).getException().get().printStackTrace();
-        // // System.out.println();
-        // TestUtils.noErrors(parserResult.getReports())
-        // TestUtils.mustFail(parserResult.getReports())
-        // var analysisResult = TestUtils.analyse(parserResult);
+        //TestUtils.parse("2+3\n10+20\n");
+        var parserResult = TestUtils.parse("2+3\n10+20\n");
+        parserResult.getReports().get(0).getException().get().printStackTrace();
+        System.out.println();
+        TestUtils.mustFail(parserResult.getReports());
+        var analysisResult = TestUtils.analyse(parserResult);
     }
 
+    @Test
+    public void testExpression2() {
+        var parserResult = TestUtils.parse("class Example { int a; }; ");
+        TestUtils.noErrors(parserResult.getReports());
+        // var analysisResult = TestUtils.analyse(parserResult);
+        // System.out.println(analysisResult);
+    }
 }
